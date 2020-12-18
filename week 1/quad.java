88ae68ca1681573fa7893e5c4f457c41e6e4fdba
Nandita2645 <71492935+Nandita2645@users.noreply.github.com>
2020-12-18 16:38:22 +0530

    Add files via upload

---
+++ week 1/quad.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.*;
+class quad
+{
+    public static void main(String[]args)
+    {
+        double a,b,c,r1=0,r2=0;
+        System.out.println("enter coefficients a,b and c of quadratic equation");
+        Scanner in=new Scanner(System.in);
+        a=in.nextFloat();
+        b=in.nextFloat();
+        c=in.nextFloat();
+        double d=(b*b)-(4*a*c);
+        if(d==0)
+        {
+            System.out.println("two equal and real roots");
+            r1=-b/2*a;
+            r2=r1;
+        }
+        else if (d>0)
+        {
+            System.out.println("two distinct real roots");
+            r1=-b+Math.sqrt(d)/2*a;
+            r2=-b-Math.sqrt(d)/2*a;
+        }
+        else
+        {
+            System.out.println("no real roots");
+            System.exit(0);
+        }
+        System.out.println("the roots of quadratic equation are r1="+r1+" and r2="+r2);
+    }
+}
